5bfc4e167f7257335928c9594ee465f081a0369e
Trieu <trieu186168@gmail.com>
2024-02-07 00:54:46 +0700

    update

--- src/Glasses/Glasses.jsx
+++ src/Glasses/Glasses.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { GlassArray } from './data';
+import { GlassArray } from './data'; // Đường dẫn đến data.js
 
 export default class Glasses extends Component {
   constructor(props) {
@@ -17,14 +17,47 @@ export default class Glasses extends Component {
     const { selectedGlass } = this.state;
 
     return (
-      <div style={{ backgroundImage: `url('./index/src/Glasses/glassesImage/background.jpg')`, backgroundSize: 'cover', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div
+        style={{
+          backgroundImage: `url('./glassesImage/background.jpg')`,
+          backgroundSize: 'cover',
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <div style={{ marginBottom: '20px' }}>
-          <img src="index/src/Glasses/glassesImage/model.jpg" alt="Model" style={{ width: '300px' }} />
-          {selectedGlass && <img src={selectedGlass.url} alt="Selected Glass" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '200px' }} />}
+          <img
+            src="./glassesImage/model.jpg"
+            alt="Model"
+            style={{ width: '300px' }}
+          />
+          {selectedGlass && (
+            <img
+              src={selectedGlass.url}
+              alt="Selected Glass"
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50% , -50%)',
+                width: '200px',
+              }}
+            />
+          )}
         </div>
+        <br /><br />
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           {GlassArray.map((glass) => (
-            <img key={glass.id} src={glass.img} alt={glass.name} style={{ width: '100px', margin: '0 10px', cursor: 'pointer' }} onClick={() => this.handleGlassSelect(glass)} />
+            <img
+              key={glass.id}
+              src={glass.img}
+              alt={glass.name}
+              style={{ width: '100px', margin: '0 10px', cursor: 'pointer' }}
+              onClick={() => this.handleGlassSelect(glass)}
+            />
           ))}
         </div>
       </div>
